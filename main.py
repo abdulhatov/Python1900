ac67a228671cfdb9b33b564b2f5465b9840fc9ef
adambek <adambek@adambeks-MacBook-Air.local>
2024-10-30 14:11:37 +0600

    fix

---
+++ main.py
@@ -0,0 +1,122 @@
+
+"""
+Name = "Bema"
+Name = "Bema 2"
+#print(Name)
+Name = "Aziz"
+print(Name)
+"""
+
+#############
+
+# x = 1
+# y = 2
+# x = 3
+# print(x)
+# y = 4
+# print(x)
+# print(y)
+
+########
+
+# x = 1
+# y = 2
+# x = y
+# print(x)
+
+
+#######
+# False = 10
+# True = 10
+# None = 10
+#
+# print(False)
+
+#########  input() ######
+
+# name = input("Атынызды жазыныз : ")
+# year = input("Туулган жылынызды жазыныз, мен сизге жашынызды айтып берем : ")
+# year = int(year)
+# x = 2024 - year
+#
+# print("Cиздин атыныз : ", name)
+# print("Cиздин жашыныз : ", x)
+
+#########  Арифметикалык амалдар #########
+"""
+ + 
+ -
+ /
+ *
+"""
+#### + ###
+
+
+
+
+
+
+
+
+
+# x = 1
+# y = 1
+# z = x + y
+#
+# print(z)
+
+
+
+
+
+
+
+
+
+####  -  ####
+
+# x = 10
+# y = 5
+# z = x - y
+# print(z)
+
+####  /  ####
+
+# x = 11
+# y = 5
+# z = x / y
+#
+# print(z)
+
+##### * ####
+
+# x = 10.2
+# y = 5
+# answer = x * y
+#
+# print(answer)
+
+#### **  ####
+# x = 10
+# y = 5
+# answer = y**x
+# print(answer)
+
+################
+
+# x = int(input("napiwi chislo"))
+# y = int(input("vtoroy chislo"))
+# answer = x**y
+# print(answer)
+
+##############
+
+
+# x = input("San kirgiziniz : ")
+# y = input("San kirgiziniz : ")
+#
+#
+#
+# print(int(y) + int(x))
+
+#####
